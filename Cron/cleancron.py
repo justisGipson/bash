3529319411ba62b711b8d246a7feb3f0e11a2df2
Justis <justis710@gmail.com>
2021-05-20 15:24:45 -0400

    add python cron job script

---
+++ Cron/cleancron.py
@@ -0,0 +1,18 @@
+from crontab import CronTab
+cron = CronTab(user='jgipson')
+
+cron.remove_all(comment="daily_clean")
+print("Job removed")
+
+job = cron.new(
+  command='bash /Users/jgipson/Cron/cleanit_job', comment='daily_clean'
+)
+
+job.setall(0, 10)
+
+cron.write()
+
+print("Job created")
+
+for job in cron:
+  print(job)
