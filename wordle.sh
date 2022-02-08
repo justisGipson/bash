c2691085ab02de4b7538cc22ff2f5bce33e38326
Justis <justis710@gmail.com>
2022-02-08 15:30:24 -0500

    wordling the wordle shell thing

--- wordle.sh
+++ wordle.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-
-mapfile -t words < <(grep -x '[a-z]\{5\}' "${WORDLIST:-/usr/share/dict/words}")
+printf '%d%d' "${CHARS:=5}" "${ROUNDS:=6}" &>/dev/null || set 1
+[[ $# != 0 ]] && { printf '[CHARS=n] [ROUNDS=n] [WORDLIST=file] %s\n' "${0##*/}" >&2; exit 1; }
+printf 'Letters: %d Rounds: %d\n' "${CHARS#-}" "$ROUNDS"
+mapfile -t words < <(grep -x '[a-z]\{'"${CHARS#-}"'\}' "${WORDLIST:-/usr/share/dict/words}")
 word=${words[RANDOM % ${#words[@]}]} pool=abcdefghijklmnopqrstuvwxyz
-for ((round=1; round <= ${ROUNDS:=6}; round++)); do
+for ((round=1; round <= $ROUNDS; round++)); do
 	while read -rp "$round/$ROUNDS: " guess || exit 1; do
-		[[ " ${words[@]} " == *" ${guess,,} "* ]] && guess=${guess,,} && break
+		[[ " ${words[*]} " == *" ${guess,,} "* ]] && guess=${guess,,} && break
 	done
 	for ((i=0, chars=0; i < ${#word}; i++)); do
-		[[ ${word:i:1} != ${guess:i:1} ]] && chars+=${word:i:1}
+		[[ ${word:i:1} != "${guess:i:1}" ]] && chars+=${word:i:1}
 	done
 	for ((i=0; color=7, i < ${#guess}; i++)); do char=${guess:i:1}
-		{ [[ ${word:i:1} == $char ]] && color=2; } ||
-		{ [[ ${chars#0} == *$char* ]] && color=3 chars=${chars/$char/}; }
-		[[ $word != *$char* ]] && pool=${pool/$char/}
+		{ [[ ${word:i:1} == "$char" ]] && color=2; } ||
+		{ [[ ${chars#0} == *"$char"* ]] && color=3 chars=${chars/$char/}; }
+		[[ $word != *"$char"* ]] && pool=${pool/$char/}
 		printf '\033[30;4%dm %s \033[0m' "$color" "${char^^}"
 	done
 	printf ' %s\n' "$pool"
-	[[ $guess == $word ]] && exit
+	[[ $guess == "$word" ]] && exit
 done
 ! printf 'The word was "%s"\n' "$word" && false
